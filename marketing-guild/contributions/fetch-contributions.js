1a1e7026ea76df8c0f09953bffe6ea46570de1d3
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-12-13 12:05:55 +0700

    refactor: improve ordering

--- marketing-guild/contributions/fetch-contributions.js
+++ marketing-guild/contributions/fetch-contributions.js
@@ -17,7 +17,7 @@ async function fetchContributions() {
         body: JSON.stringify({
             query: `
                 query ContributionsQuery {
-                    contributions(where: {circle_id: {_eq: "${coordinapeConfig.circle_id}"}}, order_by: {updated_at: desc}) {
+                    contributions(where: {circle_id: {_eq: "${coordinapeConfig.circle_id}"}}, order_by: {updated_at: desc, description: asc}) {
                         id
                         user_id
                         updated_at
