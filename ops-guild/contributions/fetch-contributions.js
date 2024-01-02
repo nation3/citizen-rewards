4a224f3b66e04fbca0bf43f8551944455d9bb921
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-01-02 15:56:22 +0700

    feat: weekly contributions - ops (#7)
    #7

--- ops-guild/contributions/fetch-contributions.js
+++ ops-guild/contributions/fetch-contributions.js
@@ -42,16 +42,39 @@ function exportToCSV(contributions) {
     const columns = [
         'id',
         'profile_id',
+        'passport_id',
         'updated_at',
+        'hours_spent',
         'description'
     ];
     const stringifier = stringify({ header: true, columns: columns });
+    const citizenDetailsJson = require('../../citizen_details_per_coordinape_profile_id.json');
     for (const contribution of contributions) {
+        const citizenDetails = citizenDetailsJson[contribution.profile_id];
+        contribution['passport_id'] = citizenDetails.citizen_passport_id;
+        contribution['hours_spent'] = extractHoursSpent(contribution.description);
         console.log('Writing contribution to CSV:', contribution);
         stringifier.write(contribution);
     }
     stringifier.pipe(writeableStream);
     console.log('Finished writing data to CSV:', filename);
+}
+
+function extractHoursSpent(contributionDescription) {
+    console.log('extractHoursSpent');
+    
+    // Look for number inside brackets ("[3.5h]" or "[2h]")
+    if ((String(contributionDescription).trim().startsWith('['))) {
+        contributionDescription = String(contributionDescription).trim();
+        contributionDescription = String(contributionDescription).substring(1, String(contributionDescription).length);
+        hoursSpent = String(contributionDescription).split(']')[0];
+        hoursSpent = String(hoursSpent).replace('h', '');
+        hoursSpent = String(hoursSpent).trim();
+        return Number(hoursSpent);
+    }
+
+    // If hours were not reported, assume 20 minutes by default
+    return 0.33;
 }
 
 fetchContributions();
